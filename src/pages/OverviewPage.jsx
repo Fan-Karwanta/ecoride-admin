e9f5fd03c01a2134f23b64218518c42a190455dc
Fan Karwanta <fankarwanta@Fans-MacBook-Air.local>
2025-03-23 16:46:28 +0800

    fixing some terms

--- src/pages/OverviewPage.jsx
+++ src/pages/OverviewPage.jsx
@@ -23,7 +23,7 @@ const OverviewPage = () => {
 					<StatCard name='Overall Total Revenue' icon={Zap} value='₱12,345' color='#6366F1' />
 					<StatCard name='New Users' icon={Users} value='1,234' color='#8B5CF6' />
 					<StatCard name='Total Bookings' icon={ShoppingBag} value='567' color='#EC4899' />
-					<StatCard name='Approvals Rate' icon={BarChart2} value='12.5%' color='#10B981' />
+					<StatCard name='Approval Rate' icon={BarChart2} value='12.5%' color='#10B981' />
 				</motion.div>
 
 				{/* CHARTS */}
